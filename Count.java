65b04f5e062fd79eac51fac71c7863fc3cfe517c
Anurag Singh <anurag8333@gmail.com>
2025-01-12 17:49:11 +0530

    Java

---
+++ Count.java
@@ -0,0 +1,7 @@
+class Count{
+    public static void main(String[] args) {
+      String words = "Hello  My Name is Anurag Singh";
+      int number = words.split("\\s+").length;
+      System.out.println(number);  
+    }
+}
