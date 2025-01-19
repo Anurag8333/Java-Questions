6faf545575faa00b50cb40859deb2fabecfb62bf
Anurag Singh <anurag8333@gmail.com>
2025-01-19 22:53:54 +0530

    Java

---
+++ Check.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+class Check {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter any value on Keyboard : ");
+        char a = s.next().charAt(0);
+        int asci = (int) a;
+        if (asci >= 65 && asci <= 90 || asci >= 97 && asci <= 122) {
+            char c = (char) asci;
+            if (c == 'a' || c == 'A' || c == 'e' || c == 'E' || c == 'i' || c == 'I' || c == 'o' || c == 'O' || c == 'u' || c == 'U') {
+                System.out.println("Vowels");
+            } else {
+                System.out.println("Consonents");
+            }
+        } else if (asci >= 48 && asci <= 57) {
+            System.out.println("Numbers");
+        } else {
+            System.out.println("Special Character");
+        }
+    }
+}
