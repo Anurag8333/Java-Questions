ca8301119fc339631f2dffaed6534c736783d07a
Anurag Singh <anurag8333@gmail.com>
2025-03-22 16:46:40 +0530

    Java

---
+++ functions/Fibonacci.java
@@ -0,0 +1,18 @@
+class Fibonacci{
+
+    public static void fibonacci(int n){
+       int a = 0;
+       int b = 1;
+       int c = 0;
+       for(int i=0; i<=n; i++){
+        System.out.print(a+" ");
+            c = a+b;
+            a=b;
+            b=c;
+       } 
+    } 
+
+    public static void main(String[] args) {
+        fibonacci(20);
+    }
+}
