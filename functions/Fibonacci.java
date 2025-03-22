1f5eafaa17847a7f72f543ba5cca74df2d310e40
Anurag Singh <anurag8333@gmail.com>
2025-03-22 16:48:22 +0530

    Java

--- functions/Fibonacci.java
+++ functions/Fibonacci.java
@@ -3,7 +3,7 @@ class Fibonacci{
     public static void fibonacci(int n){
        int a = 0;
        int b = 1;
-       int c = 0;
+       int c;
        for(int i=0; i<=n; i++){
         System.out.print(a+" ");
             c = a+b;
