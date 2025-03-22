dd179f6ae324411818707b342638165632e81848
Anurag Singh <anurag8333@gmail.com>
2025-03-22 15:54:46 +0530

    Java

--- Recursion/ReverseNum.java
+++ Recursion/ReverseNum.java
@@ -1,16 +1,14 @@
 public class ReverseNum {
     
-    public static int reverseNumber(int n, int reversed) {
-        if (n == 0) {
-            return reversed;
-        }
-        int remainder = n % 10;
-        reversed = reversed * 10 + remainder;
-        return reverseNumber(n / 10, reversed);
+    public  static int reverseNumber(int n , int reverse){
+        if(n == 0) return reverse;
+        int remainder = n %10; 
+        reverse = reverse * 10 + remainder;   
+        return reverseNumber(n/10, reverse);
     }
-    
+  
     public static void main(String[] args) {
-        int number = 123;
+        int number = 123456;
         int reversed = reverseNumber(number, 0);
         System.out.println("Reversed Number: " + reversed);
     }
