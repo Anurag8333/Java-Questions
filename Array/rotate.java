4540bfdbcd5c35c7c19da9042fb2a02c43573bcf
Anurag Singh <anurag8333@gmail.com>
2025-03-23 06:47:09 +0530

    Java

--- Array/rotate.java
+++ Array/rotate.java
@@ -1,16 +1,30 @@
 public class rotate {
+
+    public static void reverse(int []a,int st,int end){
+            while(st<end){
+             int temp =a[st];
+             a[st]=a[end];
+             a[end]=temp;
+             st++;
+             end--;
+            }
+        
+    }
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5,6};
+        
+        int k = 3; // number of rotation
+        int n = arr.length-1; // array size
+        k = k % n;
+        if(k<0){
+            k = k+n; 
+        } 
 
-        int n=3;
-        for(int i=1;i<n;i++){
-            int temp = arr[0];
-            for(int j=0; j<(arr.length-1);j++){
-                arr[j] = arr[j+1];
-            }
-            arr[arr.length-1] = temp;
-        }
-        for(int i:arr){
+        reverse(arr, 0, n);
+        reverse(arr, 0, k-1);
+        reverse(arr, k, n);
+
+        for (int i : arr) {
             System.out.print(i+" ");
         }
     }
